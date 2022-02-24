b3ba96258811c444f5ee92ded3997e4e5c4402f0
mikitaro-27 <matt.diak@gmail.com>
2022-02-24 17:16:07 +0000

    Creates tests for normal items

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -19,5 +19,46 @@ describe('Shop', () => {
         expect(item.sellIn).toEqual(-1)
       });
     });
+
+    describe('when normal item', () => {
+      describe('when in date', () => {
+        it('decreases quality by 1 and sellIn by 1', () => {
+          const item = new Item('Normal item', 5, 30)
+          const shop = new Shop([item]);
+          shop.updateQuality();
+          expect(shop.items[0].quality).toEqual(29)
+          expect(shop.items[0].sellIn).toEqual(4)
+        })
+      });
+
+      describe('when out of date', () => {
+        describe('when sellIn is 0', () => {
+          it('decreases quality by 2 and sellIn by 1', () => {
+            const item = new Item('Normal item', 0, 30)
+            const shop = new Shop([item]);
+            shop.updateQuality();
+            expect(shop.items[0].quality).toEqual(28)
+            expect(shop.items[0].sellIn).toEqual(-1)
+          });
+        });
+        
+        it('decreases quality by 2 and sellIn by 1', () => {
+          const item = new Item('Normal item', -3, 30)
+          const shop = new Shop([item]);
+          shop.updateQuality();
+          expect(shop.items[0].quality).toEqual(28)
+          expect(shop.items[0].sellIn).toEqual(-4)
+        });
+
+        describe('when quality is 0', () => {
+          it('does not decrease quality', () => {
+            const item = new Item('Normal item', -3, 0)
+            const shop = new Shop([item]);
+            shop.updateQuality();
+            expect(shop.items[0].quality).toEqual(0)
+          });
+        });
+      });
+    });
   });
 });
