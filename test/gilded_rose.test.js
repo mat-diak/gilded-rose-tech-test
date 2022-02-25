c4751daead9b26a1c03478d98f48f06f8d49ef6e
mikitaro-27 <matt.diak@gmail.com>
2022-02-25 07:56:13 +0000

    Creates tests for Backstage passes

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -29,6 +29,16 @@ describe('Shop', () => {
           expect(shop.items[0].quality).toEqual(29)
           expect(shop.items[0].sellIn).toEqual(4)
         })
+
+        describe('when quality is 0', () => {
+          it('does not decrease quality', () => {
+            const item = new Item('Normal item', 5, 0)
+            const shop = new Shop([item]);
+            shop.updateQuality();
+            expect(shop.items[0].quality).toEqual(0)
+            expect(shop.items[0].sellIn).toEqual(4)
+          });
+        });
       });
 
       describe('when out of date', () => {
@@ -109,6 +119,112 @@ describe('Shop', () => {
             expect(shop.items[0].quality).toEqual(50)
             expect(shop.items[0].sellIn).toEqual(-6)
           });
+        });
+
+        describe('when quality is 49', () => {
+          it('increases quality by 1', () => {
+            const item = new Item('Aged Brie', -5, 49)
+            const shop = new Shop([item]);
+            shop.updateQuality();
+            expect(shop.items[0].quality).toEqual(50)
+            expect(shop.items[0].sellIn).toEqual(-6)
+          });
+        });
+      });
+    });
+
+    describe('when Backstage passes', () => {
+      describe('when sellIn above 10', () => {
+        it('increases quality by 1', () => {
+          const item = new Item('Backstage passes to a TAFKAL80ETC concert', 14, 0)
+          const shop = new Shop([item]);
+          shop.updateQuality();
+          expect(shop.items[0].quality).toEqual(1)
+          expect(shop.items[0].sellIn).toEqual(13)
+        });
+
+        describe('when quality is 50', () => {
+          it('does not increase quality', () => {
+            const item = new Item('Backstage passes to a TAFKAL80ETC concert', 22, 50)
+            const shop = new Shop([item]);
+            shop.updateQuality();
+            expect(shop.items[0].quality).toEqual(50)
+            expect(shop.items[0].sellIn).toEqual(21)
+          });
+        });
+      });
+
+      describe('when sellIn is between 6..10', () => {
+        it('increases quality by 2', () => {
+          const item = new Item('Backstage passes to a TAFKAL80ETC concert', 10, 0)
+          const shop = new Shop([item]);
+          shop.updateQuality();
+          expect(shop.items[0].quality).toEqual(2)
+          expect(shop.items[0].sellIn).toEqual(9)
+        });
+
+        it('increases quality by 2', () => {
+          const item = new Item('Backstage passes to a TAFKAL80ETC concert', 6, 0)
+          const shop = new Shop([item]);
+          shop.updateQuality();
+          expect(shop.items[0].quality).toEqual(2)
+          expect(shop.items[0].sellIn).toEqual(5)
+        });
+
+        describe('when quality is 49', () => {
+          it('increases quality by 1', () => {
+            const item = new Item('Backstage passes to a TAFKAL80ETC concert', 6, 49)
+          const shop = new Shop([item]);
+          shop.updateQuality();
+          expect(shop.items[0].quality).toEqual(50)
+          expect(shop.items[0].sellIn).toEqual(5)
+          });
+        });
+      });
+
+      describe('when sellIn is 1 to 5', () => {
+        it('increases quality by 3', () => {
+          const item = new Item('Backstage passes to a TAFKAL80ETC concert', 5, 0)
+          const shop = new Shop([item]);
+          shop.updateQuality();
+          expect(shop.items[0].quality).toEqual(3)
+          expect(shop.items[0].sellIn).toEqual(4)
+        });
+
+        it('increases quality by 3', () => {
+          const item = new Item('Backstage passes to a TAFKAL80ETC concert', 1, 0)
+          const shop = new Shop([item]);
+          shop.updateQuality();
+          expect(shop.items[0].quality).toEqual(3)
+          expect(shop.items[0].sellIn).toEqual(0)
+        });
+
+        describe('when quality is 48', () => {
+          it('increases quality by 2', () => {
+            const item = new Item('Backstage passes to a TAFKAL80ETC concert', 3, 48)
+          const shop = new Shop([item]);
+          shop.updateQuality();
+          expect(shop.items[0].quality).toEqual(50)
+          expect(shop.items[0].sellIn).toEqual(2)
+          });
+        });
+      });
+
+      describe('when sellIn is 0 or less', () => {
+        it('sets quality to 0', () => {
+          const item = new Item('Backstage passes to a TAFKAL80ETC concert', 0, 30)
+          const shop = new Shop([item]);
+          shop.updateQuality();
+          expect(shop.items[0].quality).toEqual(0)
+          expect(shop.items[0].sellIn).toEqual(-1)
+        });
+
+        it('sets quality to 0', () => {
+          const item = new Item('Backstage passes to a TAFKAL80ETC concert', -5, 30)
+          const shop = new Shop([item]);
+          shop.updateQuality();
+          expect(shop.items[0].quality).toEqual(0)
+          expect(shop.items[0].sellIn).toEqual(-6)
         });
       });
     });
--- underconsideration.js
+++ underconsideration.js
@@ -1,4 +1,4 @@
 const { Shop, Item } = require("./src/gilded_rose");
 
-let item = new Item('Aged Brie', 2, 0)
+let item = new Item('Backstage passes to a TAFKAL80ETC concert', 15, 0)
 let shop = new Shop([item]);
