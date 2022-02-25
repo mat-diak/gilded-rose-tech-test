3e5c96c78d48fa2cc463a3a2dd90fc91b4c2b905
mikitaro-27 <matt.diak@gmail.com>
2022-02-25 12:16:49 +0000

    Refactor if statements in updateItem method

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -12,50 +12,45 @@ class Shop {
   }
 
   updateItems() {
-    // for each item
     for (let i = 0; i < this.items.length; i++) {
       if (!this._isSulfuras(this.items[0])) {
         this._updateItem(this.items[0]);
       }
     }
-
     return this.items;
   }
 
   _updateItem(item) {
-    if (
-      item.name != "Aged Brie" &&
-      item.name != "Backstage passes to a TAFKAL80ETC concert"
-    ) {
-        this._decreaseQuality(item);
-    } else {
+    if (item.name === "Aged Brie") {
       this._increaseQuality(item);
-      if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.sellIn < 11) {
-          this._increaseQuality(item);
-        }
-        if (item.sellIn < 6) {
-          this._increaseQuality(item);
-        }
+    } else if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
+      this._increaseQuality(item);
+      if (item.sellIn < 11) {
+        this._increaseQuality(item);
       }
+      if (item.sellIn < 6) {
+        this._increaseQuality(item);
+      }
+    } else {
+      this._decreaseQuality(item);
     }
 
     if (item.sellIn <= 0) {
-      if (item.name != "Aged Brie") {
-        if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-          this._decreaseQuality(item);
-        } else {
-          item.quality = 0;
-        }
-      } else {
+      if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
+        item.quality = 0;
+      } else if (item.name === "Aged Brie") {
         this._increaseQuality(item);
+      } else {
+        this._decreaseQuality(item);
       }
     }
+
+    // updates sell in for all items except for
     this._updateSellIn(this.items[0]);
   }
 
   _decreaseQuality(item) {
-    if(item.quality > 0) {
+    if (item.quality > 0) {
       item.quality--;
     }
   }
