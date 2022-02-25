2dd001e5d75e0ffa83ee1d2f45761216ec3a7351
mikitaro-27 <matt.diak@gmail.com>
2022-02-25 11:22:41 +0000

    Create increase/decreaseQuality methods

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -25,45 +25,27 @@ class Shop {
   }
 
   _updateItem(item) {
-    // if item is not Brie and not a backstage pass
     if (
       item.name != "Aged Brie" &&
       item.name != "Backstage passes to a TAFKAL80ETC concert"
     ) {
-      // and its quality is larger than 0
       if (item.quality > 0) {
-        // and it is not sulfuras
-          // deduct one from quality
-          console.log(`${item.name} --- 1`);
-          item.quality = item.quality - 1;
+          this._decreaseQuality(item)
       }
-      // this applies only to Aged Brie and Backstage passes
     } else {
-      // if the item has quality less than 50
       if (item.quality < 50) {
-        // add one to quality
-        console.log(`${item.name}  --- 2`);
-        item.quality = item.quality + 1;
-        // if item is backstage pass
+        this._increaseQuality(item)
         if (
           item.name == "Backstage passes to a TAFKAL80ETC concert"
         ) {
-          // and items sellIn date is less than 11
           if (item.sellIn < 11) {
-            // and it's quality is less than 50
             if (item.quality < 50) {
-              // add one to item quality
-              console.log(`${item.name} --- 3`);
-              item.quality = item.quality + 1;
+              this._increaseQuality(item)
             }
           }
-          // and if the sellIn date is less than 6
           if (item.sellIn < 6) {
-            // and the quality is less than 50
             if (item.quality < 50) {
-              // add one to item quality
-              console.log(`${item.name} --- 4`);
-              item.quality = item.quality + 1;
+              this._increaseQuality(item)
             }
           }
         }
@@ -72,42 +54,35 @@ class Shop {
     
     this._updateSellIn(this.items[0])
 
-    // if the sellIn is past
     if (item.sellIn < 0) {
-      // and item is not Aged Brie
       if (item.name != "Aged Brie") {
-        // and not a backstage pass to concert
         if (
           item.name != "Backstage passes to a TAFKAL80ETC concert"
         ) {
-          // if the quality is more than 0
           if (item.quality > 0) {
-            // if the item is not Sulfuras
-              // decrease quality by 1
-              console.log(`${item.name}  --- 6`);
-              item.quality = item.quality - 1;
+              this._decreaseQuality(item)
           }
-          // else ?? if the item is a backstage pass pass
         } else {
-          // set quality to 0
-          console.log(`${item.name} --- 7`);
-          item.quality =
-            item.quality - item.quality;
+          item.quality = 0
         }
-        // else ?? if the item is 'Aged Brie'
       } else {
-        // and the quality is less than 50
         if (item.quality < 50) {
-          // add one to quality
-          console.log(`${item.name} --- 8`);
-          item.quality = item.quality + 1;
+          this._increaseQuality(item)
         }
       }
     }
   }
 
+  _decreaseQuality(item) {
+    item.quality--
+  }
+
+  _increaseQuality(item) {
+    item.quality++
+  }
+
   _updateSellIn(item) {
-      item.sellIn--
+    item.sellIn--
   }
 
   _isSulfuras(item) {
